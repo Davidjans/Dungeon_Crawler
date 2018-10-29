07dfd4c289d90dd38e41973fddaea0cf7aab1e1c
Unknown <davidjejans@hotmail.nl>
2018-10-29 21:47:55 +0100

    .

---
+++ MedivalRoguelike/MedivalRogueLike/Assets/Game/NewScripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerState
+{
+	Idle,
+	Attacking
+}
+
+public class PlayerController : MonoBehaviour {
+	public PlayerState m_PlayerState;
+
+	[SerializeField] private Animator m_PlayerAnimator;
+
+	private void Start()
+	{
+	}
+
+
+	private void Update()
+	{
+		m_PlayerState = PlayerState.Idle;
+		if (Input.GetMouseButton(0))
+		{
+			m_PlayerState = PlayerState.Attacking;
+		}
+
+		m_PlayerAnimator.SetInteger("PlayerState", (int)m_PlayerState);
+	}
+
+	public void SetAnimation(PlayerState playerState)
+	{
+		m_PlayerState = playerState;
+	}
+}
